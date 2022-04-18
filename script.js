a213a7bc35db72dd26fb86b679da14db52745ac5
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-04-18 09:02:21 -1200

    portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function infinite() {
+    const url = `https://emerald211.github.io/infinite-scroll/`
+    window.open(url, "_blank")
+  }
+
+  function quote() {
+      const url = `https://emerald211.github.io/quote-generator/`
+      window.open(url, "_blank")
+  }
+
+  function joke() {
+      const url = `https://emerald211.github.io/JOKE-TELLER/`
+      window.open(url, "_blank")
+  }
+
+ 
